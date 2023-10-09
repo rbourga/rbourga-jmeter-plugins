a7d73ccc4e6f3e15a36de07c089e150b55a2f985
rbourga <32535546+rbourga@users.noreply.github.com>
2023-10-09 16:17:26 +0800

    Switch to CommonsCSV library instead of opencsv.

--- JmeterToTeamcity/src/main/java/com/github/rbourga/jmeter/jmx2tc/logic/Jmx2TcLogic.java
+++ JmeterToTeamcity/src/main/java/com/github/rbourga/jmeter/jmx2tc/logic/Jmx2TcLogic.java
@@ -3,98 +3,104 @@
  */
 package com.github.rbourga.jmeter.jmx2tc.logic;
 
-import com.opencsv.CSVParser;
-import com.opencsv.CSVParserBuilder;
-import com.opencsv.CSVReader;
-import com.opencsv.CSVReaderBuilder;
-import com.opencsv.CSVWriter;
-import com.opencsv.CSVWriterBuilder;
-import com.opencsv.exceptions.CsvException;
-
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVPrinter;
+import org.apache.commons.csv.CSVRecord;
 import org.apache.commons.io.FilenameUtils;
 import org.apache.jmeter.samplers.SampleSaveConfiguration;
 
 public final class Jmx2TcLogic {
 
 	public static int SelectAndRenameColumns(String sInFilepath) {
+		CSVFormat csvFmtIn, csvFmtOut;
+
 		// Initialize output file name
 		String sFileDirectoryName = FilenameUtils.getFullPath(sInFilepath);
 		String sFileBaseName = FilenameUtils.getBaseName(sInFilepath);
 		String sFileExtension = FilenameUtils.getExtension(sInFilepath);
 		String sOutFile = sFileDirectoryName + sFileBaseName + "_TC." + sFileExtension;
 
-		// Let's define the columns we want to select and their new names
-		List<String> inCol = Arrays.asList("timeStamp", "elapsed", "label", "success");
-		List<String> outCol = Arrays.asList("startTime", "spendTime", "label", "isSuccessful");
-
 		// Get the delimiter separator of the input file from current JMeter properties
 		char cDelim = SampleSaveConfiguration.staticConfig().getDelimiter().charAt(0);
-		CSVParser csvParser = new CSVParserBuilder().withSeparator(cDelim).build();
+		if (cDelim == ',') {
+			// Comma separated format
+			csvFmtIn = CSVFormat.RFC4180.builder().setHeader().setSkipHeaderRecord(true).build();
+		} else {
+			// Tab-delimited format
+			csvFmtIn = CSVFormat.TDF.builder().setHeader().setSkipHeaderRecord(true).build();
+		}
 
 		// Read the input CSV file
+		FileReader fileRdr = null;
 		try {
-			CSVReader csvRdr = new CSVReaderBuilder(new FileReader(sInFilepath))
-					.withCSVParser(csvParser)
-					.build();
-			List<String[]> inRows = null;
+			fileRdr = new FileReader(sInFilepath);
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+		Iterable<CSVRecord> inRcds = null;
+		try {
+			inRcds = csvFmtIn.parse(fileRdr);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		if (!inRcds.iterator().hasNext()) {
 			try {
-				inRows = csvRdr.readAll();
-			} catch (CsvException e) {
+				fileRdr.close();
+			} catch (IOException e) {
 				e.printStackTrace();
 			}
-			csvRdr.close();
+			return -1; // Nothing loaded, so abort...
+		}
 
-			if (inRows.isEmpty()) {
-				return -1; // Nothing loaded, so abort...
+		// Create a list to hold the data for the selected columns
+		List<String[]> outDataList = new ArrayList<>();
+
+		// Extract and store the data for selected columns
+		for (CSVRecord rcd : inRcds) {
+			String sStartTime = rcd.get("timeStamp");
+			String sSpendTime = rcd.get("elapsed");
+			String sLabel = rcd.get("label");
+			String sIsSuccessful = rcd.get("success");
+			String[] outRow = {sStartTime, sSpendTime, sLabel, sIsSuccessful};
+			outDataList.add(outRow);
+		}
+
+		// CSVParser parses the CSV data lazily; so only close the reader when done.
+		try {
+			fileRdr.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		// Define the header for the new Teamcity file & write the data to the output Teamcity file
+		String[] tcHeader = {"startTime", "spendTime", "label", "isSuccessful"};
+
+		FileWriter fileWrtr = null;
+		try {
+			fileWrtr = new FileWriter(sOutFile);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		csvFmtOut = CSVFormat.TDF.builder().setHeader(tcHeader).build();
+		try (CSVPrinter csvPrinter = new CSVPrinter(fileWrtr, csvFmtOut)) {
+			try {
+				csvPrinter.printRecords(outDataList);
+			} catch (IOException e) {
+				e.printStackTrace();
 			}
-
-			// Find the indices of the selected columns in the header row
-			String[] jmxHeaders = inRows.get(0);
-			List<Integer> selectedIndices = new ArrayList<>();
-			for (String sColName : inCol) {
-				int iColIndex = Arrays.asList(jmxHeaders).indexOf(sColName);
-				selectedIndices.add(iColIndex);
-			}
-
-			// Rename the selected columns in the header row
-			for (int i = 0; i < selectedIndices.size(); i++) {
-				int iColIndex = selectedIndices.get(i);
-				if (iColIndex != -1) {
-					jmxHeaders[iColIndex] = outCol.get(i);
-				}
-			}
-
-			// Create a list to hold the data for the selected columns
-			List<String[]> outSelectedData = new ArrayList<>();
-
-			// Extract and store the data for the selected columns
-			for (String[] row : inRows) {
-				String[] outRow = new String[inCol.size()];
-				for (int i = 0; i < inCol.size(); i++) {
-					int iColIndex = selectedIndices.get(i);
-					if (iColIndex != -1) {
-						outRow[i] = row[iColIndex];
-					}
-				}
-				outSelectedData.add(outRow);
-			}
-
-			// Write the updated header row and data rows to the output tabbed file
-			CSVWriter csvWrtr = (CSVWriter) new CSVWriterBuilder(new FileWriter(sOutFile))
-					.withQuoteChar(CSVWriter.NO_QUOTE_CHARACTER)
-					.withSeparator('\t')
-					.build();
-			csvWrtr.writeAll(outSelectedData);
-			csvWrtr.close();
-
-		}  catch (IOException e) {
+		} catch (IOException e1) {
+			e1.printStackTrace();
+		}
+		try {
+			fileWrtr.close();
+		} catch (IOException e) {
 			e.printStackTrace();
 		}
 		return 0;
